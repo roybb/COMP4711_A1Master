2052d8605b091117c184cdeef5e1818ee769579b
lockan <lockan@gmail.com>
2015-01-25 15:48:14 -0800

    template page wireframe stubbed in

---
+++ application/views/_template_main.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>title</title>
+        <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
+		<script type="text/javascript" src="../../jquery/jquery-2.1.3.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="../../css/template.css">
+    </head>
+    <body>
+		<div id="wrapper_main">
+					
+			<div class="heading_page">
+				<span id="heading_text">{{page_heading}}</span>
+			</div>
+			
+			<div id="menu_wrapper">
+			{{menu}}
+			</div>
+			
+			<div id="content" class="content_page">
+			{{content}}
+			</div>
+			
+		</div>
+    </body>
+</html>
