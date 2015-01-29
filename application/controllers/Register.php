e994263104e1e3f5aea5a5bdfa07b8de3a82487f
James Lee <jamessosailee@gmail.com>
2015-01-28 16:20:32 -0800

    Added user registration view and controller. Signed-off-by:James Lee <jamessosailee@gmail.com>

---
+++ application/controllers/Register.php
@@ -0,0 +1,23 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Register extends Main_Controller {
+
+	function __construct() {
+        parent::__construct();
+    }
+	
+	public function index()
+	{	
+		/* Set the page title, heading, and content here */
+		$this->data["pagetitle"] = "RedScribeIt User Registration";
+		$this->data["heading"] = "RedScribeIt Registration";
+		$this->data["menu"] = "menu";
+		$this->data["content"] = "register";
+		
+		/* calls Render in the Main_Controller 
+		see MY_Controller.php in ./core */
+		$this->render(); 
+	}
+}
---
+++ application/views/register.php
@@ -0,0 +1,13 @@
+<div id="content_main"> 
+	<h2>User Registration</h2>
+        <form action="">
+            <label for="username">Username:</label>
+            <input id="username" type="text" name="fname">
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" name="lname">
+            <br>
+        <input type="submit" value="Register">
+    </form> 
+</div>
+
