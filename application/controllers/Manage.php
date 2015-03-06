6ca99373adc42979b0e2d845b18c85f82f01b475
Roy <Roy@Roy-VAIO>
2015-03-05 16:36:14 -0800

    Added confirm() function to Manage controller that takes input from view, adds to database and reloads page Signed-off-by:Roy <Roy@Roy-VAIO>

--- application/controllers/Manage.php
+++ application/controllers/Manage.php
@@ -10,23 +10,29 @@ class Manage extends Main_Controller {
 
     function __construct() {
         parent::__construct();
+        //$this->load->helper('formfields');
     }
     
-    function index() {
+    public function index() {
         
              /* Set the page title, heading, and content here */
             $this->data["pagetitle"] = "RedScribeIt Manage";
             $this->data["heading"] = "Manage Subscriptions";
             $this->data["menu"] = "menu";
             $this->data["content"] = 'manage';
+            
+            $this->load->model("subscription");
 		
             /* calls Render in the Main_Controller 
             see MY_Controller.php in ./core */
             $this->render();  
     }
-    
-    function add() {
-        
+    function confirm() {
+        $record = $this->subscription->create();
+        $record->url = $this->input->post('furl');
+        $record->userid = 1;
+        $this->subscription->add($record);
+        //$this->subscription->addSub();
+        redirect('/manage');
     }
-    
 }
