2aca0f2543eecc7b583fb2938c427ca487167d75
lockan <lockan@gmail.com>
2015-03-07 14:58:41 -0800

    Avatar on main and add-subs pages

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -16,12 +16,13 @@ class Main extends Main_Controller {
 		$this->data["menu"] = "menu";
 		$this->data["content"] = "main";
 		
-		/* Set user name display and avatar img here */
+		/* Do avatar setup */
 		session_Start();
 		$user = array();
 		$user = $this->users->get($_SESSION["id"]); 	//This needs to get session var. 
 		$this->data["uname"] = $_SESSION["user"];
 		$this->data["avatar"] = "assets/images/" . $user->avatar;
+		/* End avatar setup */
 		
 		/* calls Render in the Main_Controller 
 		see MY_Controller.php in ./core */
