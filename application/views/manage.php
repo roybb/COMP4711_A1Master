05b58a3c061e7c4aebd3cc1385ad072a953f109f
Roy Banks-Bywater <rbanksbywater@gmail.com>
2015-03-08 19:33:06 -0700

    Updated the layout Signed-off-by:Roy Banks-Bywater <rbanksbywater@gmail.com>

--- application/views/manage.php
+++ application/views/manage.php
@@ -1,7 +1,9 @@
-<div id="content_main" class="content_page"> 
+<div id="content_main" class="content_page">
+    <h2>Add a subreddit subscription</h2>
     <form action = "/manage/confirm" method = "post">
-        URL: <input type="text" name="furl"><br>
-        <input type="submit" value="Submit">
+        <div class = "errors">{message}</div>
+        Subreddit URL: <input type="text" name="furl"><br><br>
+        <input type="submit" value="Add">
     </form>
 </div>
 
