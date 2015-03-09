fbc8a035db47fee928c3ad2de4c834bae41229b5
Roy Banks-Bywater <rbanksbywater@gmail.com>
2015-03-08 19:30:34 -0700

    Added subreddit url validation Signed-off-by:Roy Banks-Bywater <rbanksbywater@gmail.com>

--- application/controllers/Manage.php
+++ application/controllers/Manage.php
@@ -10,7 +10,7 @@ class Manage extends Main_Controller {
 
     function __construct() {
         parent::__construct();
-        //$this->load->helper('formfields');
+        $this->errors = array();
     }
     
     public function index() {
@@ -19,7 +19,7 @@ class Manage extends Main_Controller {
             $this->data["pagetitle"] = "RedScribeIt Manage";
             $this->data["heading"] = "Manage Subscriptions";
             $this->data["menu"] = "menu";
-            $this->data["content"] = 'manage';
+            $this->data['manage'] = 'manage';
             
             $this->load->model("subscription");
 			
@@ -31,16 +31,41 @@ class Manage extends Main_Controller {
 			$this->data["avatar"] = "assets/images/" . $user->avatar;
 			/* End avatar setup */
 		
+             $message = '';
+             if (count($this->errors) > 0)
+             {
+                 foreach($this->errors as $booboo)
+                     $message .= $booboo ;
+             }
+             $this->data['message'] = $message;
+             
+             $this->data["content"] = $this->parser->parse($this->data["manage"], $this->data, true);
+             
             /* calls Render in the Main_Controller 
             see MY_Controller.php in ./core */
-            $this->render();  
+            $this->render_manage();  
     }
     function confirm() {
+        
         session_Start();
-		$record = $this->subscription->create();
+        
+        $record = $this->subscription->create();
+        //get subscription url from form
         $record->url = $this->input->post('furl');
         $record->userid = $_SESSION['id'];
-        $this->subscription->add($record);
-        redirect('/manage');
+        
+        /*confirm user does not have more than 5 subscriptions*/
+        $ret = $this->subscription->validateSubscription($record->userid);
+        if($ret == FALSE)
+           $this->errors[] = 'Maximum number of subscriptions is 5'; 
+        else {   
+            $this->subscription->add($record);
+        }
+        if(count($this->errors) > 0) {
+            session_abort();
+            $this->index();
+        }
+        else
+            redirect('/manage');
     }
 }
